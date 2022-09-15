97cd30b261317b841b3a65b5a4e68748bd244d28
vokur13 <vokur13@gmail.com>
2022-09-15 14:40:16 +0300

    files added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './js/component-1';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
